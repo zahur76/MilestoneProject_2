89e1eda820226e1c1cdf5b777abc3a1e89ee6885
zahur76 <zahurmeerun@hotmail.com>
2020-10-14 15:35:37 +0000

    Update character ID to reflect API ID's

---
+++ assets/js/character.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".char").click(function(){
+        $("#character-info").show();
+    })
+
+    $("#reset").click(function(){
+         $("#character-info").hide();                 
+    });    
+    
+
+});
